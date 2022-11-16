39ff64db15945daca7ed140950d853f60b2c53a6
Johan Walter <walter.johan25@gmail.com>
2022-11-16 15:28:24 +0100

    exo 2 done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root
+  # get 'restaurants', to: 'restaurants#index'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+
+  resources :restaurants, only: %i[index create new show] do
+    resources :reviews, only: %i[new create]
+  end
 end
